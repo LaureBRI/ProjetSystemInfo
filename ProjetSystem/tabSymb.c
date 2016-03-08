5230cab214d4195c148a8db42a9107f19c808fea
LaureBRI <lau-ure@hotmail.fr>
2016-03-08 11:17:18 +0100

    fixe tabSymb

--- ProjetSystem/tabSymb.c
+++ ProjetSystem/tabSymb.c
@@ -17,11 +17,6 @@ struct chmpSymb buildEntry(char * n, int d, int a, int i, int c){
 
 // Ajoute la structure elem à la liste tab
 void addEntry(struct chmpSymb elem, struct tabSymb * tab){
-	struct tabSymb * courant;
-	struct tabSymb * next;
-	struct tabSymb * temp;
-	next = tab;
-
 	if (tab == NULL){
 		tab = malloc(sizeof(struct tabSymb))
 		tab->head = malloc(sizeof(struct cellTabSymb));
@@ -31,48 +26,44 @@ void addEntry(struct chmpSymb elem, struct tabSymb * tab){
 	}
 	else
 	{
-		while(next != NULL){
-			courant = next;
-			next = courant->next;
-		}
-		temp = malloc(sizeof(struct tabSymb));
-		temp->elem = elem;
-		temp->next = NULL;
-		courant->next = temp;
+		tab->tail->next = malloc(sizeof(struct cellTabSymb));
+		tab->tail = tab->tail->next;
+		tab->tail->elem = elem;
+		tab->tail->next = NULL;
 	}
 }
 
 // Peut être retourner un int pour savoir si la pile était déjà vide au départ
 //Supprime le dernier élément de la liste tab
 void supprEntry(struct tabSymb * tab){
-	struct tabSymb * courant;
-	struct tabSymb * next;
-	next = tab;
+	struct cellTabSymb * courant;
+	struct cellTabSymb * next;
+	next = tab->head;
 
 /*	if (tab == NULL){
 		return -1
 	}
 	else
 */
-	while(next->next != NULL){
-		courant = next;
-		next = courant->next;
+	while(next->next != tab->tail){
+		next = next->next;
 	}
-	courant->next = NULL;
-	free(next);
+	tab->tail = next;
+	free(tab->tail->next);
+	tab->tail->next = NULL;
 }
 
 //Trouve une entrée dans la table des symboles par son nom
 struct chmpSymb * findEntry(char * n, struct tabSymb * tab){
-	struct tabSymb * courant;
-	courant = tab;
+	struct cellTabSymb * courant;
+	courant = tab->head;
 
-	if(tab == NULL){
+	if(tab->head == NULL){
 		return NULL;
 	}
 	else
 	{
-		while(strcmp(courant->elem.name, n) != 0 || courant->next != NULL){
+		while(strcmp(courant->elem.name, n) != 0 || courant != tab->tail){
 			courant = courant->next;
 		}
 		return &(courant->elem);
